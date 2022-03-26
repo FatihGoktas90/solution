202bf6254cb160d58ae024a118a8d00b9ea0648e
Fatih <goktasfatih90@gmail.com>
2022-03-26 18:33:09 +0300

    "finish project"

--- solution/src/components/card/Card.js
+++ solution/src/components/card/Card.js
@@ -1,25 +1,20 @@
 
-import './Card.css';
-import { languages } from '../../helpers/data';
-import Item from '../item/Item';
+import { languages } from "../../helpers/data";
+import Item from "../item/Item";
+import "./Card.css";
 
+const Card = () => {
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h1 className="language-title">Languages</h1>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} key={index} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
-const Card = ()=> {
-    return (
-        <div className='cards-area-container'>
-            {/* <div className='bars'></div> */}
-            <h1 className='language-title'>Languages</h1>
-            {
-                languages.map((item,index)=> {
-                    return(
-                      <Item card={item} key={index}/>
-                    )
-                }
-                )
-                
-            }
-        </div>
-    )
-}
-
-export default Card
+export default Card;
