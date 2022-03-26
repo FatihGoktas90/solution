e3a90f2a17df6fd6ad3473d156a3254f0618c8b1
Fatih <goktasfatih90@gmail.com>
2022-03-26 18:01:29 +0300

    updated

---
+++ solution/src/components/item/Item.js
@@ -0,0 +1,16 @@
+import './Item.css';
+
+const Item =({card})=> {
+    return(
+        <div className="card">
+            <img className="card-logo" src={card.img}  alt="logo" />
+            <h3 className="card-title" >{card.name}</h3>
+            {
+                // console.log(card)
+            }
+
+        </div>
+    )
+}
+
+export default Item
